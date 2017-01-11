cd1a98c51343fe9841c955cd2d1a0ff903d572e9
Casey Schneebeck <cschneebeck@tnc.org>
2017-01-11 10:39:31 -0700

    clean up

--- main.js
+++ main.js
@@ -37,17 +37,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 				// Hide the print button until a hex has been selected
 				$(this.printButton).hide();
 			}else{
-				this.map.addLayer(this.dynamicLayer);
-<<<<<<< HEAD
-=======
-				//this.map.addLayer(this.category);
-				// on set state it calls activate twice. on the second call render is true so it call this else. layer infos isn't done yet so if you call setNavBtns it can't use layer infos
-				if (this.obj.stateSet == "no"){	
-					//this.navigation.setNavBtns(this);	
-				}else{
-					this.obj.stateSet = "no";
-				}	
->>>>>>> origin/development
+				this.map.addLayer(this.dynamicLayer);	
 			}	
 			this.open = "yes";
 		},
