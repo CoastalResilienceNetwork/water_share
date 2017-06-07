8be1dcc2ecd104993e245d72664cd0543b3bd7d7
Casey Schneebeck <cschneebeck@tnc.org>
2017-06-07 13:10:22 -0600

    dev url

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 			declare.safeMixin(this, frameworkParameters);
 			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
 			this.obj = dojo.eval("[" + obj + "]")[0];	
-			this.url = "http://dev.services2.coastalresilience.org:6080/arcgis/rest/services/Water_Blueprint/water_share/MapServer";
+			this.url = "http://dev.services.coastalresilience.org:6080/arcgis/rest/services/Water_Blueprint/Water_Scarcity/MapServer";
 			this.layerDefs = [];
 		},
 		// Called after initialize at plugin startup (why all the tests for undefined). Also called after deactivate when user closes app by clicking X. 
